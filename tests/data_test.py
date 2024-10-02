39af07f24346a8e576f8dbc69f000b83665136d7
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2024-10-02 14:19:29 +0100

    tests: convert to use pytest

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-import unittest
+
 import numpy as np
 from rex_ai.input_data import Data
 
 
 tab = np.arange(0, 1, 999)
 
-
-class TestData(unittest.TestCase):
-    
-    def test_data(self):
-        data = Data(input=tab, model_shape=[1, 999], device="cpu")
-        self.assertEqual(data.model_shape, [1, 999])
-        self.assertEqual(data.mode, "spectral")
+def test_data():
+    data = Data(input=tab, model_shape=[1, 999], device="cpu")
+    assert data.model_shape == [1, 999]
+    assert data.mode == "spectral"
