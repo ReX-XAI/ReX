fd8df513d7e436ff4932610de0b0288a8847d9bd
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2024-12-09 09:51:49 +0000

    tests: update model file link to a permalink

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from cached_path import cached_path
 
 @pytest.fixture
 def resnet50():
-    resnet50_path = cached_path("https://github.com/onnx/models/raw/refs/heads/main/validated/vision/classification/resnet/model/resnet50-v1-7.onnx")
+    resnet50_path = cached_path("https://github.com/onnx/models/blob/bec48b6a70e5e9042c0badbaafefe4454e072d08/validated/vision/classification/resnet/model/resnet50-v1-7.onnx")
     return resnet50_path
