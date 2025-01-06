6c27f36cac25798ca9cee3bd5850fe3cd7c27ca1
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-01-06 10:47:35 +0000

    rework explanation tests to use fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,13 @@ import pytest
 
 from cached_path import cached_path
 
+from rex_xai.config import CausalArgs, process_custom_script
+from rex_xai._utils import get_device
+from rex_xai.explanation import (
+    try_preprocess,
+    get_prediction_func_from_args,
+)
+
 
 @pytest.fixture
 def resnet50():
@@ -9,3 +16,40 @@ def resnet50():
         "https://github.com/onnx/models/raw/bec48b6a70e5e9042c0badbaafefe4454e072d08/validated/vision/classification/resnet/model/resnet50-v1-7.onnx"
     )
     return resnet50_path
+
+
+@pytest.fixture
+def args():
+    args = CausalArgs()
+    args.path = "tests/test_data/dog.jpg"
+    process_custom_script("tests/scripts/pytorch.py", args)
+
+    return args
+
+
+@pytest.fixture
+def model_shape(args):
+    prediction_func, model_shape = get_prediction_func_from_args(args)
+
+    return model_shape
+
+
+@pytest.fixture
+def prediction_func(args):
+    prediction_func, model_shape = get_prediction_func_from_args(args)
+
+    return prediction_func
+
+
+@pytest.fixture
+def data(args, model_shape, cpu_device):
+    data = try_preprocess(args, model_shape, device=cpu_device)
+
+    return data
+
+
+@pytest.fixture
+def cpu_device():
+    device = get_device(gpu=False)
+
+    return device
--- tests/explanation_test.py
+++ tests/explanation_test.py
@@ -1,39 +1,29 @@
 import pytest
 
-from rex_xai.config import CausalArgs, process_custom_script
-from rex_xai._utils import get_device
 from rex_xai.explanation import (
     try_preprocess,
     predict_target,
-    get_prediction_func_from_args,
     load_and_preprocess_data,
     validate_args,
 )
 
-args = CausalArgs()
-args.path = "imgs/dog.jpg"
-process_custom_script("tests/scripts/pytorch.py", args)
-device = get_device(gpu=False)
 
-prediction_func, model_shape = get_prediction_func_from_args(args)
-data = try_preprocess(args, model_shape, device=device)
-
-
-def test_preprocess(snapshot):
+def test_preprocess(args, model_shape, cpu_device, snapshot):
     args.custom = None
-    data = load_and_preprocess_data(model_shape, device, args)
+    data = load_and_preprocess_data(model_shape, cpu_device, args)
     assert data == snapshot
 
 
-def test_preprocess_custom(snapshot):
-    process_custom_script("tests/scripts/pytorch.py", args)
-    data = load_and_preprocess_data(model_shape, device, args)
+def test_preprocess_custom(args, model_shape, cpu_device, snapshot):
+    data = load_and_preprocess_data(model_shape, cpu_device, args)
     assert data == snapshot
 
 
-def test_preprocess_spectral_mask_on_image_returns_warning(caplog, snapshot):
+def test_preprocess_spectral_mask_on_image_returns_warning(
+    args, model_shape, cpu_device, snapshot, caplog
+):
     args.mask_value = "spectral"
-    data = try_preprocess(args, model_shape, device=device)
+    data = try_preprocess(args, model_shape, device=cpu_device)
 
     assert args.mask_value == 0
     assert (
@@ -43,33 +33,33 @@ def test_preprocess_spectral_mask_on_image_returns_warning(caplog, snapshot):
     assert data == snapshot
 
 
-def test_preprocess_nii_notimplemented(caplog):
-    args.path = "imgs/dog.nii"
-    data = try_preprocess(args, model_shape, device=device)
+def test_preprocess_nii_notimplemented(args, model_shape, cpu_device, caplog):
+    args.path = "tests/test_data/dog.nii"
+    data = try_preprocess(args, model_shape, device=cpu_device)
 
     assert data == NotImplemented
     assert caplog.records[0].msg == "we do not (yet) handle nifti files generically"
 
 
-def test_preprocess_npy(caplog, snapshot):
-    args.path = "imgs/DoublePeakClass 0 Mean.npy"
-    try_preprocess(args, model_shape, device=device)
+def test_preprocess_npy(args, model_shape, cpu_device, snapshot, caplog):
+    args.path = "tests/test_data/DoublePeakClass 0 Mean.npy"
+    try_preprocess(args, model_shape, device=cpu_device)
 
     assert caplog.records[0].msg == "we do not generically handle this datatype"
 
     args.mode = "tabular"
-    data = try_preprocess(args, model_shape, device=device)
+    data = try_preprocess(args, model_shape, device=cpu_device)
     assert data == snapshot
 
 
-def test_predict_target(snapshot):
+def test_predict_target(data, prediction_func):
     target = predict_target(data, prediction_func)
 
     assert target.classification == 207
     assert target.confidence == pytest.approx(0.253237, abs=2.5e-6)
 
 
-def test_validate_args():
+def test_validate_args(args):
     args.path = None  #  type: ignore
     with pytest.raises(FileNotFoundError):
         validate_args(args)
