474425c87b8af737a9842d6d22ae0d031383c04a
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-01-09 14:59:48 +0000

    tests: simplify visualisation testing to just confirm output is produced without error

--- tests/visualisation_test.py
+++ tests/visualisation_test.py
@@ -1,25 +1,24 @@
-from syrupy.extensions.image import PNGImageSnapshotExtension
-
+import os
 
 def test_surface(exp_extracted, tmp_path, snapshot):
     p = tmp_path / "surface.png"
     exp_extracted.surface_plot(path=p)
 
-    with open(p, "rb") as ifile:
-        assert snapshot(extension_class=PNGImageSnapshotExtension) == ifile.read()
+    assert os.path.exists(p)
+    assert os.stat(p).st_size > 0
 
 
 def test_heatmap(exp_extracted, tmp_path, snapshot):
     p = tmp_path / "heatmap.png"
     exp_extracted.heatmap_plot(path=p)
 
-    with open(p, "rb") as ifile:
-        assert snapshot(extension_class=PNGImageSnapshotExtension) == ifile.read()
+    assert os.path.exists(p)
+    assert os.stat(p).st_size > 0
 
 
 def test_save_exp(exp_extracted, tmp_path, snapshot):
     p = tmp_path / "exp.png"
     exp_extracted.save(path=p)
 
-    with open(p, "rb") as ifile:
-        assert snapshot(extension_class=PNGImageSnapshotExtension) == ifile.read()
+    assert os.path.exists(p)
+    assert os.stat(p).st_size > 0
