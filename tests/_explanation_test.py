24d5e1eca785090d21704881c5a416c4879b56ac
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-01-15 10:39:00 +0000

    tests: test multiple values for batch parameter

--- tests/_explanation_test.py
+++ tests/_explanation_test.py
@@ -5,10 +5,11 @@ from syrupy.extensions.amber.serializer import AmberDataSerializer
 from syrupy.filters import props
 from syrupy.matchers import path_type
 
-
+@pytest.mark.parametrize("batch", [1, 64])
 def test__explanation_snapshot(
-    args_custom, model_shape, prediction_func, cpu_device, snapshot
+    args_custom, model_shape, prediction_func, cpu_device, batch, snapshot
 ):
+    args_custom.batch = batch
     exp = _explanation(args_custom, model_shape, prediction_func, cpu_device, db=None)
 
     assert (
@@ -29,9 +30,12 @@ def test__explanation_snapshot(
         )
     )
 
+@pytest.mark.parametrize("batch", [1, 64])
 def test__explanation_snapshot_diff_model_shape(
-    args_torch_swin_v2_t, model_shape_swin_v2_t, prediction_func_swin_v2_t, cpu_device, snapshot
+    args_torch_swin_v2_t, model_shape_swin_v2_t, prediction_func_swin_v2_t, cpu_device, batch, snapshot
 ):
+    args_torch_swin_v2_t.batch = batch
+
     exp = _explanation(args_torch_swin_v2_t, model_shape_swin_v2_t, prediction_func_swin_v2_t, cpu_device, db=None)
 
     assert (
