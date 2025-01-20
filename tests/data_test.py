0a23658c0de0a7e166a7836e31d0ddd9090caa4c
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-01-20 14:31:38 +0000

    tests: test that spectral occlusion can only be used for spectral data

--- tests/data_test.py
+++ tests/data_test.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from rex_xai.input_data import Data
+from rex_xai.explanation import load_and_preprocess_data
 
 
 tab = np.arange(0, 1, 999)
@@ -11,3 +12,11 @@ def test_data():
     data = Data(input=tab, model_shape=[1, 999], device="cpu")
     assert data.model_shape == [1, 999]
     assert data.mode == "spectral"
+
+
+def test_set_mask_value(args_custom, model_shape, cpu_device, caplog):
+    data = load_and_preprocess_data(model_shape, cpu_device, args_custom)
+    data.set_mask_value('spectral', device=data.device)
+    
+    assert caplog.records[0].msg == "Mask value 'spectral' can only be used if mode is also 'spectral', using default mask value 0 instead"
+    assert data.mask_value == 0
