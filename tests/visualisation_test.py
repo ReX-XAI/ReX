da757b11daa2b8d4e381721cefb4ff6ff3bb6c9a
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-01-22 14:12:04 +0000

    tests: remove unused argument

--- tests/visualisation_test.py
+++ tests/visualisation_test.py
@@ -1,6 +1,6 @@
 import os
 
-def test_surface(exp_extracted, tmp_path, snapshot):
+def test_surface(exp_extracted, tmp_path):
     p = tmp_path / "surface.png"
     exp_extracted.surface_plot(path=p)
 
@@ -8,7 +8,7 @@ def test_surface(exp_extracted, tmp_path, snapshot):
     assert os.stat(p).st_size > 0
 
 
-def test_heatmap(exp_extracted, tmp_path, snapshot):
+def test_heatmap(exp_extracted, tmp_path):
     p = tmp_path / "heatmap.png"
     exp_extracted.heatmap_plot(path=p)
 
@@ -16,7 +16,7 @@ def test_heatmap(exp_extracted, tmp_path, snapshot):
     assert os.stat(p).st_size > 0
 
 
-def test_save_exp(exp_extracted, tmp_path, snapshot):
+def test_save_exp(exp_extracted, tmp_path):
     p = tmp_path / "exp.png"
     exp_extracted.save(path=p)
 
