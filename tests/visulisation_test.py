ba44f4b4fddef1258220a2a6808be152ed3d5627
melane6 <mnavaratnarajah6@gmail.com>
2025-01-29 10:23:33 +0000

    tests: add interactive voxel plot test

--- tests/visulisation_test.py
+++ tests/visulisation_test.py
@@ -2,6 +2,7 @@
 import os
 
 import numpy as np
+from PIL.Image import Image
 
 from rex_xai.visualisation import voxel_plot, save_image
 from rex_xai.config import CausalArgs
@@ -9,7 +10,8 @@ from rex_xai.input_data import Data
 
 import torch as tt
 
-voxel = np.random.rand(1, 64, 64, 64)
+voxel = np.zeros(( 1, 64, 64, 64), dtype=np.float32)
+voxel[0:30, 20:30, 20:35] = 1
 data_3d = Data(input=voxel, model_shape=[1, 64, 64, 64], device="cpu")
 data_3d.mode = "voxel"
 data_3d.data = voxel
@@ -28,3 +30,12 @@ def test_save_image_3d():
     assert os.path.getsize(args.output) > 0
 
     os.remove(args.output)
+
+def test_voxel_plot():
+    resp_map = np.zeros((64, 64, 64), dtype=np.float32)
+    resp_map[0:10, 20:25, 20:35] = 1
+    # Create a cube in data
+    voxel_plot(resp_map, data_3d, path="test.png")
+    assert os.path.exists("test.png")
+    assert os.path.getsize("test.png") > 0
+    os.remove("test.png")
