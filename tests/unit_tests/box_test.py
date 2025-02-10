83ea1f85cf5fa9d503c4cb9d5c0c343a5af81100
melane6 <mnavaratnarajah6@gmail.com>
2025-02-10 15:21:55 +0000

    fix: add seed for splitting of boxes

--- tests/unit_tests/box_test.py
+++ tests/unit_tests/box_test.py
@@ -46,6 +46,8 @@ def test_initialise_tree_2d(box_2d):
     )  # TODO: Box_dimensions has the same functionality as corners, should we remove one of them.
 
 def test_spawn_children_3d(box_3d, resp_map_3d):
+    # Set seed
+    np.random.seed(24)
     children_3d = box_3d.spawn_children(min_size=20, mode="voxel", map=resp_map_3d)
     assert len(children_3d) == 4
     assert children_3d[0].area() < 262144
@@ -58,11 +60,34 @@ def test_spawn_children_3d(box_3d, resp_map_3d):
     )
     assert total_area_3d == 262144
 
+    volumes = [1536, 2560, 96768, 161280]
+
+    # Check splitting of boxes
+    row_split, col_split = 1, 24
+
+    row_starts = [0, 0, row_split, row_split]
+    row_stops = [row_split, row_split, 64, 64]
+    col_starts = [0, col_split, 0, col_split]
+    col_stops = [col_split, 64, col_split, 64]
+
     for i in range(4):
         assert children_3d[i].name == f"R:{i}"
+        # Not split through depth
+        assert children_3d[i].depth_start == 0
+        assert children_3d[i].depth_stop == 64
+
+        assert children_3d[i].area() == volumes[i]
+
+        assert children_3d[i].row_start == row_starts[i]
+        assert children_3d[i].row_stop == row_stops[i]
+        assert children_3d[i].col_start == col_starts[i]
+        assert children_3d[i].col_stop == col_stops[i]
+
 
 
 def test_spawn_children_2d(box_2d, resp_map_2d):
+    # Set seed
+    np.random.seed(24)
     children_2d = box_2d.spawn_children(min_size=2, mode="RGB", map=resp_map_2d)
     assert len(children_2d) == 4
     assert children_2d[0].area() < 4096
@@ -74,6 +99,24 @@ def test_spawn_children_2d(box_2d, resp_map_2d):
         + children_2d[3].area()
     )
     assert total_area_2d == 4096
+    print(children_2d)
+    areas = [210, 174, 2030, 1682]
+
+    # Check splitting of boxes
+    row_split, col_split = 6, 35
+
+    row_starts = [0, 0, row_split, row_split]
+    row_stops = [row_split, row_split, 64, 64]
+    col_starts = [0, col_split, 0, col_split]
+    col_stops = [col_split, 64, col_split, 64]
 
     for i in range(4):
         assert children_2d[i].name == f"R:{i}"
+        assert children_2d[i].area() == areas[i]
+
+        assert children_2d[i].row_start == row_starts[i]
+        assert children_2d[i].row_stop == row_stops[i]
+        assert children_2d[i].col_start == col_starts[i]
+        assert children_2d[i].col_stop == col_stops[i]
+
+
