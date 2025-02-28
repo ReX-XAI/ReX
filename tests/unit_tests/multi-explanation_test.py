c20c92a0e96c48b0486821453f9d3511b2196f76
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-02-28 15:59:26 +0000

    tests: add test for saving multi-explanations

--- tests/unit_tests/multi-explanation_test.py
+++ tests/unit_tests/multi-explanation_test.py
@@ -1,9 +1,10 @@
 import logging
+import os
 
 import numpy as np
 import pytest
 from rex_xai._utils import Strategy
-from rex_xai.explanation import calculate_responsibility
+from rex_xai.explanation import calculate_responsibility, _explanation
 from rex_xai.multi_explanation import MultiExplanation
 
 
@@ -36,3 +37,32 @@ def test_multiexplanation(data_multi, args_multi, prediction_func, spotlights, c
     assert np.array_equal(
         multi_exp.explanations[0].detach().cpu().numpy(), exp.final_mask
     )  # first explanation is global explanation
+
+
+def test_multiexplanation_save_composite(args_multi, model_shape, prediction_func, cpu_device, tmp_path):
+    exp = _explanation(args_multi, model_shape, prediction_func, cpu_device, db=None)
+    clauses = exp.separate_by(0.0)
+
+    p = tmp_path / "exp.png"
+    exp.save(path=p, multi_style="composite", clauses=None)
+
+    assert os.path.exists(p)
+    assert os.stat(p).st_size > 0
+
+    exp.save(path=p, multi_style="composite", clauses=clauses)
+    for c in clauses:
+        clause_path = tmp_path /f"exp_{c}.png"
+        assert os.path.exists(clause_path)
+        assert os.stat(clause_path).st_size > 0
+
+
+def test_multiexplanation_save_separate(args_multi, model_shape, prediction_func, cpu_device, tmp_path):
+    exp = _explanation(args_multi, model_shape, prediction_func, cpu_device, db=None)
+
+    p = tmp_path / "exp.png"
+    exp.save(path=p, multi_style="separate")
+
+    for i in range(len(exp.explanations)):
+        exp_path = tmp_path /f"exp_{i}.png"
+        assert os.path.exists(exp_path)
+        assert os.stat(exp_path).st_size > 0
