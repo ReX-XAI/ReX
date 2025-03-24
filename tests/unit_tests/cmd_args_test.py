b4f84c993aa7f854d2a6d520e245bce3e49ee0af
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-03-24 14:39:25 +0000

    tests: add test for --quiet option

--- tests/unit_tests/cmd_args_test.py
+++ tests/unit_tests/cmd_args_test.py
@@ -1,46 +1,42 @@
-import pytest
 from types import ModuleType
 
+import pytest
 from rex_xai._utils import Strategy
-from rex_xai.config import CausalArgs, shared_args, cmdargs_parser, process_cmd_args
+from rex_xai.config import CausalArgs, cmdargs_parser, process_cmd_args, shared_args
 
-
-# filename (path)
-# output (path)
-# config (path)
-# processed (bool)
-# script (path)
-# verbose (int)
-# quiet (bool)
-# surface (path)
-# heatmap (path)
-# model (path)
-# strategy (multi | spatial | global | spotlight)
-# database (path)
-# multi (int)
 # contrastive (int)
-# iters (int)
-# analyze/analyse (bool)
-# mode (<tabular>, <spectral>, <RGB>, <voxel>, <audio>)
 # spectral (bool)
+
 
 @pytest.fixture
 def non_default_cmd_args():
-    args_list = ['filename.jpg',
-        '--output', 'output_path.jpg',
-        '--config', 'path/to/rex.toml',
-        '--processed',
-        '--script', 'tests/scripts/pytorch_resnet50.py',
-        '-vv',
-        '--surface', 'surface_path.jpg',
-        '--heatmap', 'heatmap_path.jpg',
-        '--model', 'path/to/model.onnx',
-        '--strategy', 'multi',
-        '--database', 'path/to/database.db',
-        '--multi', '5',
-        '--iters', '10',
-        '--analyze',
-        '--mode', 'RGB',
+    args_list = [
+        "filename.jpg",
+        "--output",
+        "output_path.jpg",
+        "--config",
+        "path/to/rex.toml",
+        "--processed",
+        "--script",
+        "tests/scripts/pytorch_resnet50.py",
+        "-vv",
+        "--surface",
+        "surface_path.jpg",
+        "--heatmap",
+        "heatmap_path.jpg",
+        "--model",
+        "path/to/model.onnx",
+        "--strategy",
+        "multi",
+        "--database",
+        "path/to/database.db",
+        "--multi",
+        "5",
+        "--iters",
+        "10",
+        "--analyze",
+        "--mode",
+        "RGB",
     ]
     parser = cmdargs_parser()
     cmd_args = parser.parse_args(args_list)
@@ -56,10 +52,10 @@ def test_process_cmd_args(non_default_cmd_args):
     assert args.path == non_default_cmd_args.filename
     assert args.strategy == Strategy.MultiSpotlight
     assert args.iters == int(non_default_cmd_args.iters)
-    assert args.analyze 
+    assert args.analyze
     assert args.spotlights == int(non_default_cmd_args.multi)
 
-    
+
 def test_process_shared_args(non_default_cmd_args):
     args = CausalArgs()
     shared_args(non_default_cmd_args, args)
@@ -74,3 +70,12 @@ def test_process_shared_args(non_default_cmd_args):
     assert args.mode == non_default_cmd_args.mode
     assert args.processed == non_default_cmd_args.processed
 
+
+def test_quiet_overrides_verbose():
+    cmd_args_list = ["filename.jpg", "-vv", "--quiet"]
+    parser = cmdargs_parser()
+    cmd_args = parser.parse_args(cmd_args_list)
+    args = CausalArgs()
+    shared_args(cmd_args, args)
+
+    assert args.verbosity == 0
