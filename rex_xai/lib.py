61939eeaf4b62138daa341a1da219b4472337cfa
David Kelly <dkellino@gmail.com>
2025-06-20 14:47:50 +0100

    simple ecg example

--- rex_xai/lib.py
+++ rex_xai/lib.py
@@ -4,6 +4,7 @@ from rex_xai.explanation.explanation import Explanation
 from rex_xai.explanation.rex import calculate_responsibility, predict_target
 from rex_xai.input.config import CausalArgs
 from rex_xai.input.input_data import Data
+from rex_xai.output import visualisation
 from rex_xai.responsibility.prediction import default_prediction_function
 
 
@@ -24,9 +25,24 @@ class ReX:
         self.explanation = None
         self.prediction_function = prediction_function
         self.data = None
+        self.maps = None
+        self.stats = None
 
         if self.prediction_function is None:
             self.get_default_prediction_function()
+
+    def set_tabular_data(self, path, data=None):
+        if self.mode == "spectral":
+            self.args.path = path
+            self.args.mode = "spectral"
+            if data is not None:
+                self.data = Data(
+                    data,
+                    data.shape,
+                    self.device,
+                    mode=self.args.mode,
+                    process=True,
+                )
 
     def set_rgb_image(self, path):
         if self.mode == "RGB":
@@ -62,9 +78,18 @@ class ReX:
         if self.data is not None:
             if self.data.mask_value is None:
                 self.data.set_mask_value(args.mask_value)
-            return calculate_responsibility(self.data, args, self.prediction_function)
+            maps, stats = calculate_responsibility(
+                self.data, args, self.prediction_function
+            )
+            self.maps = maps
+            self.stats = stats
+            return maps, stats
 
-    def get_explanation(self, maps, stats):
+    def get_explanation(self, maps=None, stats=None):
+        if maps is None:
+            maps = self.maps
+        if stats is None:
+            stats = self.stats
         if self.data is not None:
             self.explanation = Explanation(
                 maps, self.prediction_function, self.data, self.args, stats
@@ -78,3 +103,12 @@ class ReX:
 
     def rerun_with(self, new_args: CausalArgs):
         self.calculate_responsibility(args=new_args)
+
+    def show(self):
+        if self.mode == "spectral":
+            visualisation.spectral_plot(
+                self.explanation.sufficiency_mask,
+                self.data,
+                self.maps.get(self.data.target.classification),
+                self.args.heatmap_colours,
+            )
